d6c09f760521a44d65cf964de26a8e63c3f2f818
Mubbassir Khan Jahagirdar <khanmkj96@gmail.com>
2026-05-05 15:53:05 +0530

    SEO Improved from 57

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
 
 const container = document.getElementById('root');
 
-if (container && container.hasChildNodes()) {
-	hydrateRoot(container, <React.StrictMode><App /></React.StrictMode>);
-} else if (container) {
+if (container) {
 	const root = createRoot(container);
-	root.render(<React.StrictMode><App /></React.StrictMode>);
+	root.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
 }
