b202dbc002f39a632887c3c6650073805790aff2
Mubbassir Khan Jahagirdar <khanmkj96@gmail.com>
2026-05-07 10:37:36 +0530

    feat: implemented Github actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+vite.config.js 
+
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // Or vue, etc.
+import ghPages from 'vite-plugin-gh-pages' // Import the plugin
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/'
+  plugins: [react(), ghPages()],
+  base: '/ecovation/', // 👈 IMPORTANT: Replace 'your-repo-name' with your actual GitHub repo name
 })
