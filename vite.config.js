e5f5e2527a0014a98e0e73b56d13be91c5ec4946
Mubbassir Khan Jahagirdar <khanmkj96@gmail.com>
2026-05-07 10:46:05 +0530

    fix: Updated Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-vite.config.js 
-
 
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Or vue, etc.
-import ghPages from 'vite-plugin-gh-pages' // Import the plugin
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/ecovation/', // 👈 IMPORTANT: Replace 'your-repo-name' with your actual GitHub repo name
+  plugins: [react()],
+  base: '/'
 })
